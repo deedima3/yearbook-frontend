8ddba7f289c6bfab86422c0b9d0833400e6c2f38
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-05-04 12:16:01 +0800

    [ feature ] Add SEO and Twits

--- components/Card/TwitsCard.tsx
+++ components/Card/TwitsCard.tsx
@@ -1,19 +1,70 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
 
-interface Props{
-    title : string
-    postedOn : string
-    description : string
-    upvoted : boolean
-    downvoted : boolean
-    upvoteCount : number
-    downvoteCount : number
+interface Props {
+  title: string;
+  postedOn: string;
+  description: string;
+  upvoted: boolean;
+  downvoted: boolean;
+  upvoteCount: number;
+  downvoteCount: number;
 }
 
-const TwitsCard = ({title, postedOn, description, upvoted, upvoteCount, downvoted, downvoteCount} : Props) => {
+const TwitsCard = ({
+  title,
+  postedOn,
+  description,
+  upvoted,
+  upvoteCount,
+  downvoted,
+  downvoteCount,
+}: Props) => {
+  const [upHovered, setupHovered] = useState(false);
+  const [downHovered, setdownHovered] = useState(false);
+
+  const truncatedDescription = useMemo(() => {
+    return description.substring(0, 300);
+  }, [description]);
+
   return (
-    <div>TwitsCard</div>
-  )
-}
+    <div className="flex border-2 border-black bg-white py-10 px-10 max-w-screen-lg justify-between gap-4">
+      <div className="flex flex-col max-w-screen-md">
+        <div className="text-3xl font-bold">{title}</div>
+        <div className="text-md">{`Posted On : ${postedOn}`}</div>
+        <div className="text-lg">{truncatedDescription}</div>
+      </div>
+      <div className="flex flex-col gap-10">
+        <div className="flex gap-4 items-center">
+          <div
+            className="w-16 h-16 hover:-translate-x-1 hover:-translate-y-1 transition-all duration-200"
+            onMouseLeave={() => setupHovered(false)}
+            onMouseEnter={() => setupHovered(true)}
+          >
+            {upvoted || upHovered ? (
+              <img src="filledUp.png" alt="FilledUpvote" />
+            ) : (
+              <img src="unfilledUp.png" alt="UnFilledUpvote" />
+            )}
+          </div>
+          <p className="text-2xl font-bold text-brand-green">{upvoteCount}</p>
+        </div>
+        <div className="flex gap-4 items-center">
+          <div
+            className="w-16 h-16 hover:-translate-x-1 hover:-translate-y-1 transition-all duration-200"
+            onMouseLeave={() => setdownHovered(false)}
+            onMouseEnter={() => setdownHovered(true)}
+          >
+            {downvoted || downHovered ? (
+              <img src="filledDown.png" alt="FilledDownvote" />
+            ) : (
+              <img src="unfilledDown.png" alt="UnFilledDownvote" />
+            )}
+          </div>
+          <p className="text-2xl font-bold text-brand-red">{downvoteCount}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TwitsCard
+export default TwitsCard;
